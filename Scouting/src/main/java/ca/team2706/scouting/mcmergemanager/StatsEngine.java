1132c79f1b4197b734e573494b58ddc85872d6b1
Mike Ounsworth <ounsworth@gmail.com>
2016-02-04 15:36:16 -0500

    Added a class StatsEngine

---
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/StatsEngine.java
@@ -0,0 +1,22 @@
+package ca.team2706.scouting.mcmergemanager;
+
+import android.util.StateSet;
+
+import ca.team2706.scouting.mcmergemanager.datamodels.MatchData;
+
+/**
+ * Created by mike on 04/02/16.
+ */
+public class StatsEngine {
+
+    MatchData matchData;
+
+    /** Contructor **/
+    public StatsEngine(MatchData matchData) {
+        this.matchData = matchData;
+    }
+
+    
+
+
+}
