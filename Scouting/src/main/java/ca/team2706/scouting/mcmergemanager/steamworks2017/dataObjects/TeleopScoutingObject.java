f4f719f0bc4b26fbb9978b883bae0c48533df766
Mike Ounsworth <ounsworth@gmail.com>
2017-01-29 02:32:50 -0500

    #68 Another adjustment to the data objects.

--- Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/steamworks2017/dataObjects/TeleopScoutingObject.java
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/steamworks2017/dataObjects/TeleopScoutingObject.java
@@ -9,30 +9,30 @@ public class TeleopScoutingObject {
 
     public static double MATCH_TIME = 0; // TODO: get match time in milliseconds?
 
-    public List<FuelPickupEvent> fuelPickups;
-    public List<FuelShotEvent> fuelShots;
-    public List<GearPickupEvent> gearPickups;
-    public List<GearDelivevryEvent> gearDelivevries;
 
-    public double fuelCycleTime; // TODO: [MikeO] ??
-    public double gearCycleTime; // TODO: [MikeO] ??
+//    public double fuelCycleTime; // TODO: [MikeO] ??
+//    public double gearCycleTime; // TODO: [MikeO] ??
+
+
+    private ArrayList<Event> events = new ArrayList<>();
+
+    public void add(Event e) {
+        events.add(e);
+    }
+
+    /**
+     * Puts all events during teleop mode into a list, sorted by timestamp.
+     * Useful for doing cycle analysis in StatsEngine.
+     */
+    public ArrayList<Event> getEvents() {
+        Collections.sort(events);
+
+        return events;
+    }
 
 
     public TeleopScoutingObject() {
-        fuelPickups = new ArrayList<FuelPickupEvent>();
-        fuelShots = new ArrayList<FuelShotEvent>();
-        gearPickups = new ArrayList<GearPickupEvent>();
-        gearDelivevries = new ArrayList<GearDelivevryEvent>();
-    }
 
-    public TeleopScoutingObject(List<FuelPickupEvent> fuelPickups, List<FuelShotEvent> fuelShots, List<GearPickupEvent> gearPickups,
-                                List<GearDelivevryEvent> gearDelivevries) {
-        this.fuelPickups = fuelPickups;
-        this.fuelShots = fuelShots;
-        this.gearPickups = gearPickups;
-        this.gearDelivevries = gearDelivevries;
-
-//        cycleTime();
     }
 
     // TODO probably not going to be moved to stat engine
@@ -69,30 +69,4 @@ public class TeleopScoutingObject {
 //        fuelCycleTime = fuelPickupCycleTime + fuelShotCycleTime;
 //    }
 
-
-    /**
-     * Puts all events during teleop mode into a list, sorted by timestamp.
-     * Useful for doing cycle analysis in StatsEngine.
-     */
-    public ArrayList<Event> getEvents() {
-        ArrayList<Event> events = new ArrayList<>();
-
-        // throw all the events in, then sort.
-
-        for (Event e : fuelPickups)
-            events.add(e);
-
-        for (Event e : fuelShots)
-            events.add(e);
-
-        for (Event e : gearPickups)
-            events.add(e);
-
-        for (Event e : gearDelivevries)
-            events.add(e);
-
-        Collections.sort(events);
-
-        return events;
-    }
 }
