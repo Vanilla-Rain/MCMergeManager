4a67ce917ae6de42cf89542a8ed1b3d9dc0e02df
Alden Peeters <starbounder1024@gmail.com>
2017-02-09 21:04:11 -0500

    Added some stuff for submitting data
    Isn't working, but it's basic Java stuff with object casting & other
    junk, nothing ridiculous the android studio is doing

--- Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/gui/RepairTimeCollection.java
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/gui/RepairTimeCollection.java
@@ -6,18 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import org.json.JSONException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.team2706.scouting.mcmergemanager.R;
 import ca.team2706.scouting.mcmergemanager.backend.dataObjects.MatchSchedule;
+import ca.team2706.scouting.mcmergemanager.backend.dataObjects.RepairTimeObject;
 
 /**
  * Created by alden on 2017-02-04.
  */
 
 public class RepairTimeCollection extends AppCompatActivity {
+
+    private ArrayList<View> rows = new ArrayList<View>();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +60,67 @@ public class RepairTimeCollection extends AppCompatActivity {
         TextView teamNumberTV = (TextView) curRow.findViewById(R.id.teamNumber);
         teamNumberTV.setText(teamNumber);
 
+        rows.add(curRow);
         return curRow;
+
+    }
+
+    public void onSubmitClicked(View view) {
+
+        RepairTimeObject[] repairObjects = generateRepairTimeObjects();
+
+        // TODO: Send RepairTimeObjects to... somewhere?
+
+    }
+
+    public RepairTimeObject[] generateRepairTimeObjects() {
+
+        ArrayList<RepairTimeObject> repairObjects = new ArrayList<RepairTimeObject>();
+
+        // Loop through rows
+        for(View curRow: rows) {
+
+            RepairTimeObject repairObject = null;
+
+            // Generate a repair object
+            try {
+                repairObject = generateRepairTimeObject(curRow);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // Add repair object if it exists
+            if(repairObject != null) {
+                repairObjects.add(repairObject);
+            }
+
+        }
+
+        return (RepairTimeObject[]) repairObjects.toArray();
+
+    }
+
+    public RepairTimeObject generateRepairTimeObject(View row) throws JSONException {
+
+        // Get views & stuff
+        TextView teamNumber = (TextView) row.findViewById(R.id.teamNumber);
+        RadioButton notAvailable = (RadioButton) row.findViewById(R.id.notAvaiable);
+        RadioButton yes = (RadioButton) row.findViewById(R.id.yes);
+        RadioButton no = (RadioButton) row.findViewById(R.id.no);
+
+        // Determine which radio button was selected
+        if(notAvailable.isChecked()) {
+            return new RepairTimeObject(Integer.parseInt("" + teamNumber.getText()), RepairTimeObject.RepairStatus.NOT_AVAILABLE);
+        }
+        else if(yes.isChecked()) {
+            return new RepairTimeObject(Integer.parseInt("" + teamNumber.getText()), RepairTimeObject.RepairStatus.REPAIRING);
+        }
+        else if(no.isChecked()) {
+            return new RepairTimeObject(Integer.parseInt("" + teamNumber.getText()), RepairTimeObject.RepairStatus.WORKING);
+        }
+
+        // If none were selected, return null
+        return null;
 
     }
 
