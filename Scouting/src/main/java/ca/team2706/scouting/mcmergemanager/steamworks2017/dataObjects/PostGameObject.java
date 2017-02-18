dfe32a9c1c80301130c8c3e5a017876438e840d9
Mike Ounsworth <ounsworth@gmail.com>
2017-02-18 09:13:53 -0500

    Small tweak to constructor.

--- Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/steamworks2017/dataObjects/PostGameObject.java
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/steamworks2017/dataObjects/PostGameObject.java
@@ -14,15 +14,17 @@ public class PostGameObject {
     public ClimbType climbType;
     public double climb_time;
     public double time_dead;
+    public double time_defending;
 
 
     // empty constructor
     public PostGameObject() {}
 
-    public PostGameObject(String notes, ClimbType climbType, double climb_time, double time_dead) {
+    public PostGameObject(String notes, ClimbType climbType, double climb_time, double time_dead, double time_defending) {
         this.climbType = climbType;
         this.notes = notes;
         this.climb_time = climb_time;
         this.time_dead = time_dead;
+        this.time_defending = time_defending;
     }
 }
