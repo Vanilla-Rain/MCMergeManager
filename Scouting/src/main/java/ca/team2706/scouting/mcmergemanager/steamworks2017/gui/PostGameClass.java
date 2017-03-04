bd99bdf41c7b06df5309fa39ed07b884198d1056
JustinToft <justintoft@hotmail.com>
2017-03-04 14:20:30 -0500

    Fixed issues with notes

--- Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/steamworks2017/gui/PostGameClass.java
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/steamworks2017/gui/PostGameClass.java
@@ -29,7 +29,8 @@ public class PostGameClass extends AppCompatActivity {
     private PostGameObject postGameObject;
     private DefenseEvent defenseEvent = new DefenseEvent();
 
-
+    public String notesText;
+    public String noEntry = "Notes...";
     SeekBar deadTimeSeekBar;
     SeekBar defenseSeekBar;
 
@@ -42,12 +43,11 @@ public class PostGameClass extends AppCompatActivity {
         // Using this  onClickListener so the text disappears when clicked.
         final EditText notes = (EditText) findViewById(R.id.postGameNotes);
         notes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    notes.setText("");
-                }
-            });
-
+            @Override
+            public void onClick(View view) {
+                notes.setText("");
+            }
+        });
 
 
         postGameObject = (PostGameObject) getIntent().getSerializableExtra("PostGameData");  // climb was set in climbingFragment.
@@ -69,7 +69,7 @@ public class PostGameClass extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 TextView tv = (TextView) findViewById(R.id.time_dead_text_view);
                 tv.setText(progressChangedValue * 5 + " seconds dead");
-                postGameObject.time_dead = progressChangedValue*5;
+                postGameObject.time_dead = progressChangedValue * 5;
             }
         });
 
@@ -89,7 +89,7 @@ public class PostGameClass extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 TextView tv = (TextView) findViewById(R.id.time_defending_text_view);
                 tv.setText(progressChangedValue * 5 + " seconds defending");
-                postGameObject.time_defending = progressChangedValue*5;
+                postGameObject.time_defending = progressChangedValue * 5;
 
             }
         });
@@ -98,11 +98,24 @@ public class PostGameClass extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                notesText = notes.getText().toString();
+                if (!notesText.equals(noEntry)) {
+                    postGameObject.notes = notes.getText().toString();
+
+                }
+
                 returnHome();
             }
         });
 
-        postGameObject.notes = notes.getText().toString();
+
+
+        notesText = notes.getText().toString();
+        if (!notesText.equals(noEntry)) {
+            postGameObject.notes = notes.getText().toString();
+
+        }
     }
 
         public void returnHome(){
